6b74c1cd7aee6278feba7c71260b2a3049e87754
unknown <Robotech@G3R305C001.andijan.itpark>
2023-11-17 16:58:13 +0500

    ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,10 @@
 import React from 'react';
-import Dars14 from './Dars/14-dars/Dars14';
-import SinglePage from './Dars/14-dars/SinglePage';
-import { Routes, Route, Link } from 'react-router-dom';
-import Post from './Dars/14-dars/Post';
-import Edit from './Dars/14-dars/Edit';
-import Todo from './Vazifa/todo/Todo';
-// f6_frontend
+import Dars15 from './Dars/15-dars/Dars15';
 
 const App = () => {
   return ( 
     <React.Fragment>
-      {/* <div className="navbar">
-        <Link to={"/"}>Home</Link>
-        <Link to={"/post"}>Post</Link>
-      </div>
-      <Routes>
-        <Route index element={<Dars14 />}/>
-        <Route path='/news/:id' element={<SinglePage/>}/>
-        <Route path='/edit/:id' element={<Edit/>}/>
-        <Route path='/post' element={<Post/>}/>
-      </Routes> */}
-      <Todo/>
-      
+      <Dars15/>
     </React.Fragment>
    );
 }
---
+++ src/Dars/15-dars/Dars15.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Button } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu';
+
+const Dars15 = () => {
+    return (
+        <div>
+            <button >Knopka</button>
+            <Button href='https://google.com'>Knopka 1</Button>
+            <Button variant='outlined'><MenuIcon/></Button>
+            <Button variant='contained'>Knopka 3</Button>
+        </div>
+    )
+}
+
+export default Dars15
